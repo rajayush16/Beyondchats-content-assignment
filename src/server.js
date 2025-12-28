8187683094a51bd8ec6d7a4949236da2866a7134
rajayush16 <ayush.raj12516@gmail.com>
2025-12-28 21:21:48 +0530

    Enable CORS for frontend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const articlesRouter = require("./routes/articles");
 
@@ -10,6 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_ORIGIN || "http://localhost:5173",
+  })
+);
 
 app.use("/api/articles", articlesRouter);
 
